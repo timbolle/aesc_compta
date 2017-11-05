336dabe24daadf6857c3f8d1adafd8ef03fbf0ba
timothy.bolle <timbolle01@gmail.com>
2017-11-05 17:02:32 +0100

    modif models pour que solde soit update avec transactionsmodif models

--- compta/models.py
+++ compta/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.dispatch import receiver
 
 class Compte(models.Model):
     nom = models.CharField(max_length = 140)
@@ -24,10 +25,32 @@ class Budget(models.Model):
 class Transaction(models.Model):
     nom = models.CharField(max_length=140)
     compte = models.ForeignKey(Compte, on_delete=models.CASCADE)
-    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE, blank=True, null=True)
     somme = models.DecimalField(max_digits=11, decimal_places=2)
     date = models.DateField()
     description = models.TextField()
 
     def __str__(self):
         return self.nom
+
+
+@receiver(models.signals.post_save, sender=Transaction)
+def execute_after_save(sender, instance, created, *args, **kwargs):
+    if created:
+        c = Compte.objects.get(nom=instance.compte)
+        c.somme_actuelle += instance.somme  # change field
+        c.save()  # this will update only
+        if  instance.budget:
+            b = Budget.objects.get(nom=instance.budget)
+            b.somme_actuelle += instance.somme  # change field
+            b.save()  # this will update only
+
+@receiver(models.signals.pre_delete, sender=Transaction)
+def execute_before_delete(sender, instance, using, *args, **kwargs):
+    c = Compte.objects.get(nom=instance.compte)
+    c.somme_actuelle -= instance.somme  # change field
+    c.save()  # this will update only
+    if instance.budget:
+        b = Budget.objects.get(nom=instance.budget)
+        b.somme_actuelle -= instance.somme  # change field
+        b.save()  # this will update only
