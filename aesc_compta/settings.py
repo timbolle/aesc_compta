127df527331465a468e41b9aa8791ffb68c680b6
Tim <tim4science@gmail.com>
2017-12-25 23:26:21 +0100

    loggin pages and test user (loggin restriction not done)

--- aesc_compta/settings.py
+++ aesc_compta/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Authentication
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- aesc_compta/urls.py
+++ aesc_compta/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+# from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^', include('compta.urls')),
 ]
